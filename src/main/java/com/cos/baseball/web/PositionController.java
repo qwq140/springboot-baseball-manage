82f45a10cc17a9e594355b55777969a4956a23d0
unknown <qwq140@gmail.com>
2021-03-25 22:09:39 +0900

    add README

--- src/main/java/com/cos/baseball/web/PositionController.java
+++ src/main/java/com/cos/baseball/web/PositionController.java
@@ -22,13 +22,9 @@ public class PositionController {
 	
 	@GetMapping("/position")
 	public String findByPositionPivot(Model model){
-		System.out.println("여기1");
 		JpaResultMapper result = new JpaResultMapper();
-		System.out.println("여기4");
 		Query q = em.createNativeQuery("select position, MAX(IF(teamId = 5, name, \\\"\\\")) kia, MAX(IF(teamId = 6, name, \\\"\\\")) nc, MAX(IF(teamId = 7, name, \\\"\\\")) lotte FROM player GROUP BY position");
-		System.out.println("여기5");
 		List<PositionRespDto> resultList = result.list(q, PositionRespDto.class);
-		System.out.println("여기6");
 		model.addAttribute("positions", resultList);
 		
 		return "position/positionList";
